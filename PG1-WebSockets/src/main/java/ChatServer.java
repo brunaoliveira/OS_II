7c42a3e00d1780863ba3520784934334c61daee0
brunaoliveira <brunaol678@gmail.com>
2020-10-01 18:21:22 -0300

    add imports

--- PG1-WebSockets/src/main/java/ChatServer.java
+++ PG1-WebSockets/src/main/java/ChatServer.java
@@ -1,4 +1,9 @@
 import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
+import java.util.logging.Logger;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 
 @ServerEndpoint("/chat")
 public class ChatServer{
