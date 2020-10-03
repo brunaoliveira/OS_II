8b4de81fa8457466ce008514088058aebf211bb2
topeixoto <taiane.o.p@gmail.com>
2020-10-03 13:24:17 -0300

    Create add and multiplication methods on rest class

--- PG2-WebServices/webservice/Calculadora.java
+++ PG2-WebServices/webservice/Calculadora.java
@@ -1,5 +1,8 @@
 package webservice;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Calculadora {
 
     private String operador;
--- PG2-WebServices/webservice/CalculadoraRest.java
+++ PG2-WebServices/webservice/CalculadoraRest.java
@@ -1,5 +1,28 @@
 package webservice;
 
-public class CalculadoraRest {   
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 
+
+@Path("calculadora")
+public class CalculadoraRest {
+
+    @Path("/somarInt/{a}/{b}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Calculadora somarInt(@PathParam("a") int a, @PathParam("b") int b) {
+        Calculadora soma = new Calculadora(a, b, "+");
+        return soma;
+    }
+
+    @Path("/multiplicarInt/{a}/{b}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Calculadora multiplicarInt(@PathParam("a") int a, @PathParam("b") int b) {
+        Calculadora produto = new Calculadora(a, b, "*");
+        return produto;
+    }
 }
