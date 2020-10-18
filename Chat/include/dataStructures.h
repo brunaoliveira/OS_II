2e5eeedf1daa7238ca330ef1d901d6ac46026253
brunaoliveira <brunaol678@gmail.com>
2020-10-18 16:28:29 -0300

    add TODO in dataStructures

--- Chat/include/dataStructures.h
+++ Chat/include/dataStructures.h
@@ -1,14 +1,17 @@
 #ifndef DATASTRUCTURES_H
 #define DATASTRUCTURES_H
 
+#include <iostream> // pra reconhecer o uint16_t
 
-// conforme a definição do trabalho
-typedef struct __packet{
+// conforme a definição do trabalho: packet é enviado entre servidor e cliente
+typedef struct __packet {
     uint16_t type; //Tipo do pacote (p.ex. DATA | CMD)
     uint16_t seqn; //Número de sequência
     uint16_t length; //Comprimento do payload
     uint16_t timestamp; //Timestamp do dado
     const char* _payload; //Dados da mensagem
 } packet; 
+
+// TODO: criar estrutura para as mensagens que serão trocadas entre os usuários
 
 #endif
