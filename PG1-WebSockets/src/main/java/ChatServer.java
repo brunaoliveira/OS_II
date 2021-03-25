2a2107af519948ac785f0e48be13d12e6d07cfbc
topeixoto <taiane.o.p@gmail.com>
2021-03-25 12:22:09 -0300

    handle errors on chat server when it sends messages

--- PG1-WebSockets/src/main/java/ChatServer.java
+++ PG1-WebSockets/src/main/java/ChatServer.java
@@ -10,31 +10,30 @@ import java.io.*;
 @ServerEndpoint("/chat")
 public class ChatServer {
     private static final Logger LOGGER = Logger.getLogger(ChatServer.class.getName());
-    private static ArrayList<Session> openSessions = new ArrayList<Session>(); // open sessions
-    // ou implementar com set? assim não repete identificador
-    // Set<Session> openSessions = new HashSet<Session>();
-
+    private static ArrayList<Session> openSessions = new ArrayList<Session>();
     
     @OnOpen
     public void openSession(Session newSession) {
         openSessions.add(newSession);
-        // se adicionou ao set a nova sessão 
         LOGGER.info("The session " + newSession.getId() + " was created.");
-        // log de erro se não adicionou?
     }
 
     
     @OnClose
     public void closeSession(Session sessionClosing) {
-        openSessions.remove(sessionClosing);
         LOGGER.info("The session " + sessionClosing.getId() + " was closed.");
+        openSessions.remove(sessionClosing);
     }
 
 
     @OnMessage
     public void sendMessage(String msg) throws IOException {
         for (Session session : openSessions) {
-            session.getBasicRemote().sendText(msg);
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch(IOException e){
+                System.err.println(e.getMessage());
+            }
         }
     }
     
